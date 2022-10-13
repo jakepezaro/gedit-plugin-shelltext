b3ad436d8d1b6b8114c65844c724425fafe481a6
Jake Pezaro <jake.pezaro@gmail.com>
2022-10-13 22:43:56 +0800

    wip basic menu structure

--- shelltext/__init__.py
+++ shelltext/__init__.py
@@ -1,18 +1,27 @@
-from gi.repository import GObject, Gedit
+import gi
+gi.require_version('Gedit', '3.0')
+gi.require_version('Gtk', '3.0')
 
-class ExamplePyPlugin(GObject.Object, Gedit.WindowActivatable):
-    __gtype_name__ = "ExamplePyPlugin"
+from gi.repository import GLib, Gio, GObject, Gtk, Gdk, Gedit
 
-    window = GObject.property(type=Gedit.Window)
+class AppActivatable(GObject.Object, Gedit.AppActivatable):
+    __gtype_name__ = "ExternalToolsAppActivatable"
+
+    app = GObject.Property(type=Gedit.App)
 
     def __init__(self):
         GObject.Object.__init__(self)
+        self.menu = None
 
     def do_activate(self):
-        print('activate')
+        self.tools_section = self.extend_menu("tools-section-1")
+        external_tools_submenu = Gio.Menu()
+        item = Gio.MenuItem.new_submenu(_("ZZZZZZZExternal ToolsXXX"), external_tools_submenu)
+        external_tools_submenu_section = Gio.Menu()
+        external_tools_submenu.append_section(None, external_tools_submenu_section)
+        self.tools_section.append_menu_item(item)
+        self.menu = external_tools_submenu_section
 
     def do_deactivate(self):
-        print('deactivate')
-
-    def do_update_state(self):
-        pass
+        self.menu.remove_all()
+        self.tools_section = None
