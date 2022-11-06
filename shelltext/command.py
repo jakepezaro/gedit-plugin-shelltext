5b67a88b43f83b8ec1c7060c320be538da28c788
Jake Pezaro <jake.pezaro@gmail.com>
2022-11-07 04:54:43 +0800

    refactoring

--- shelltext/command.py
+++ shelltext/command.py
@@ -6,27 +6,29 @@ def parse_command(command):
     current = ''
     quoted = None
     for c in command:
-       q = c if c in ['"', "'"] else None
-       is_separator = c in [' ', '\n', '\r']
-       is_pipe = c == '|'
-       if not quoted and q:
-           quoted = q
-           if current.strip() != '':
-               commands.append(current)
-           current = ''
-           continue
-       if quoted and quoted == q:
-           quoted = None
-           if current != '':
-               commands.append(current)
-           current = ''
-           continue
-       if is_separator and not quoted:
-           if current != '':
-               commands.append(current)
-           current = ''
-           continue
-       current += c
+        q = c if c in ['"', "'"] else None
+        is_separator = c in [' ', '\n', '\r']
+        is_pipe = c == '|'
+        if quoted:
+            if quoted == q:
+                quoted = None
+                if current != '':
+                    commands.append(current)
+                current = ''
+                continue
+        else:
+            if q:
+                quoted = q
+                if current.strip() != '':
+                    commands.append(current)
+                current = ''
+                continue
+            elif is_separator:
+                if current != '':
+                    commands.append(current)
+                current = ''
+                continue
+        current += c
     if current != '':
         commands.append(current)
     return [commands]
